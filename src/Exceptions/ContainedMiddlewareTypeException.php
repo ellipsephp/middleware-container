32032ee1ca7e63ced317013594cf1560814c29a5
pmall <pierre.mallinjoud@gmail.com>
2018-03-19 17:08:35 +0100

    use type-errors package for exception messages

--- src/Exceptions/ContainedMiddlewareTypeException.php
+++ src/Exceptions/ContainedMiddlewareTypeException.php
@@ -6,16 +6,14 @@ use TypeError;
 
 use Psr\Http\Server\MiddlewareInterface;
 
+use Ellipse\Exceptions\ContainerEntryTypeErrorMessage;
+
 class ContainedMiddlewareTypeException extends TypeError implements ContainerMiddlewareExceptionInterface
 {
     public function __construct(string $id, $value)
     {
-        $template = "The value contained in the '%s' entry of the container is of type %s - object implementing %s expected";
+        $msg = new ContainerEntryTypeErrorMessage($id, $value, MiddlewareInterface::class);
 
-        $type = is_object($value) ? get_class($value) : gettype($value);
-
-        $msg = sprintf($template, $id, $type, MiddlewareInterface::class);
-
-        parent::__construct($msg);
+        parent::__construct((string) $msg);
     }
 }
